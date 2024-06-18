6f9851f08f3b2d081b5acae9f99f179fe0159ac1
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-06-18 16:45:24 -0300

    :sparkles: feat: criando estrutura de diretórios e inicializando a estilização de componentes com TailwindCSS

--- src/components/wrapper/wrapper.tsx
+++ src/components/wrapper/wrapper.tsx
@@ -1,9 +1,9 @@
+import React from 'react';
 
-export default function Wrapper({...children}) {
+export default function Wrapper({ children }: { children: React.ReactNode }) {
   return (
     <section className="block mx-auto my-10 h-screen">
       {children}
-      </section>
-          
-  )
+    </section>
+  );
 }
