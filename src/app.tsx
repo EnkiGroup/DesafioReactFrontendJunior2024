e297965c57ae23ff31b2204e0530145c4a107698
Henrique Santiago <henrique11santiagopires@hotmail.com>
2024-06-19 11:46:34 -0300

    First commit with initial settings, added SASS dependency and started app.tsx code

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+import "./styles/scss/styles.scss";
 
 export default function App() {
+  const [newTask, setNewTask] = useState<string>('')
+  
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      console.log('Enter pressionado, valor do input:', newTask);
+    }
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTask(e.target.value)
+  }
+
   return (
-    <section>
-      <h1>Todos</h1>
+    <section className="main_container_app">
+      <h1 id="main_title">Todos</h1>
+      <main id="main_container_table">
+        <div id="input_task_container">
+          <input type="text" name="" id="" placeholder="Insira uma nova tarefa" onChange={handleChange} onKeyDown={handleKeyDown}  />
+        </div>
+      </main>
+      <ul>
+
+      </ul>
     </section>
   );
 }
