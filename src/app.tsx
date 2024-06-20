1030d2d68c8f1ddab68fbc443964d029a25c2a5a
VitorDalBelo <vitoralmeida721@gmail.com>
2024-06-20 16:26:56 -0300

    variavel de ambiente

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { API_URL } from "./config";
 
 export default function App() {
   return (
     <section>
-      <h1>Todos</h1>
+      <h1>Todos{API_URL}</h1>
     </section>
   );
 }
