cb0e9aa234a70f98f65e6e83743212dbc5a21306
devenzonascimento <dev.enzody@gmail.com>
2024-06-21 17:20:59 -0300

    feat: implements filter button component to tasks footer

--- src/components/tasks-footer.tsx
+++ src/components/tasks-footer.tsx
@@ -1,5 +1,6 @@
-import { Link } from "react-router-dom"
 import { useTasksContext } from "../contexts/tasks-context"
+
+import { FilterButton } from "./filter-button"
 
 export function TasksFooter() {
 
@@ -8,35 +9,28 @@ export function TasksFooter() {
   return (
     <>
       {!isTasksListEmpty && (
-        <header className="flex justify-between px-4 py-2 border-t border-gray-200 sheets-effect">
+        <footer className="flex justify-between px-4 py-2 border-t border-gray-200 sheets-effect">
           <span>{tasksLeft} itens left!</span>
-          <div className="flex gap-4">
-            <Link
-              to={"/"}
-              className="hover:border-red-700 border border-transparent px-2 rounded-[3px] cursor-pointer"
-            >
-              All
-            </Link>
-            <Link
-              to={"/active"}
-              className="hover:border-red-700 border border-transparent px-2 rounded-[3px] cursor-pointer"
-            >
-              Active
-            </Link>
-            <Link
-              to={"/completed"}
-              className="hover:border-red-700 border border-transparent px-2 rounded-[3px] cursor-pointer"
-            >
-              Completed
-            </Link>
-          </div>
+          <nav>
+            <ul className="flex gap-4">
+              <li>
+                <FilterButton route={"/"} filterName="All" />
+              </li>
+              <li>
+                <FilterButton route={"/active"} filterName="Active" />
+              </li>
+              <li>
+                <FilterButton route={"/completed"} filterName="Completed" />
+              </li>
+            </ul>
+          </nav>
           <button
             onClick={clearCompletedTasks}
             className="hover:underline cursor-pointer"
           >
             Clear completed
           </button>
-        </header>
+        </footer>
       )}
     </>
   )
