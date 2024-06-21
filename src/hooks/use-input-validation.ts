5ceaa51846f5e9408593f00d386029644bbee384
devenzonascimento <dev.enzody@gmail.com>
2024-06-20 21:30:33 -0300

    feat: add a custom hook to validate and submit a new task

---
+++ src/hooks/use-input-validation.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTasksContext } from "../contexts/tasks-context";
+
+const taskSchema = z.object({
+  title: z.string().min(2),
+});
+
+type TaskSchema = z.infer<typeof taskSchema>;
+
+export function useInputValidation() {
+  const { register, handleSubmit, reset } = useForm<TaskSchema>({
+    resolver: zodResolver(taskSchema),
+  });
+  
+  const { addTask } = useTasksContext()
+  
+  const onSubmit = ({title}: TaskSchema) => {
+    addTask(title)
+
+    reset();
+  };
+
+
+
+  return { register, handleSubmit: handleSubmit(onSubmit) };
+}
