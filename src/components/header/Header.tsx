32dc81bea6d33fb962a13f185b9957297f4f9ae5
Wesleyoliveira01 <Wesleyoliveira.dev@outlook.com>
2024-06-21 15:57:59 -0300

    feat: creating static header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,12 @@ import Input from "../ui/input/Input";
 
 const Header = () => {
   return (
-    <header className="flex flex-col items-center justify-center gap-4 w-[35%]">
-      <h1 className="text-title-color text-7xl">todos</h1>
-      <Input />
+    <header className="flex flex-col items-center justify-center gap-4">
+      <h1 className="text-title-color text-8xl">todos</h1>
+      <Input
+        input_cn="focus:shadow-focus border p-default placeholder:italic placeholder:text-2xl placeholder:font-light shadow-inner"
+        placeholder="What needs to be done?"
+      />
     </header>
   );
 };
