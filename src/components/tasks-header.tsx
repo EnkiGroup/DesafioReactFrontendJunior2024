53e778c00814cfd10dba6fa7674cfb90a50a30e4
devenzonascimento <dev.enzody@gmail.com>
2024-06-20 21:41:43 -0300

    feat: add a task header component, with a input to add a new task (initial)

---
+++ src/components/tasks-header.tsx
@@ -0,0 +1,23 @@
+import { ChevronDown } from "lucide-react"
+import { useInputValidation } from "../hooks/use-input-validation"
+
+export function TasksInput() {
+
+  const { register, handleSubmit } = useInputValidation()
+
+  
+
+  return (
+    <form onSubmit={handleSubmit} className="flex bg-[#FEFEFE]">
+      <button className="px-2">
+        <ChevronDown color="#777" size={28} />
+      </button>
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        {...register("title")}
+        className=" w-full px-2 py-4 text-2xl placeholder:italic placeholder:text-2xl placeholder:font-normal"
+      />
+    </form>
+  )
+}
