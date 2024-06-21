2eaa967bf700f577c4645eb12282f4116af16638
devenzonascimento <dev.enzody@gmail.com>
2024-06-21 14:48:39 -0300

    feat: add a readable task item (initial)

---
+++ src/components/readable-task-item.tsx
@@ -0,0 +1,33 @@
+import { useTasksContext } from "../contexts/tasks-context";
+
+import { CheckButton } from "./check-button";
+import { X } from "lucide-react";
+
+import { Task } from "../types";
+
+interface ReadableTaskItemProps {
+  task: Task
+}
+
+export function ReadableTaskItem({ task }: ReadableTaskItemProps) {
+
+  const { toggleTaskCheck, removeTask } = useTasksContext()
+
+  function handleCheck() {
+    toggleTaskCheck(task.id)
+  }
+
+  function handleRemove() {
+    removeTask(task.id)
+  }
+
+  return (
+    <>
+      <CheckButton isDone={task.isDone} onCheck={handleCheck} />
+      <span className={`w-full px-2 py-4 text-2xl ${task.isDone ? "text-gray-400 line-through" : ""}`}>
+        {task.title}
+      </span>
+      <X className="remove-icon" onClick={handleRemove} />
+    </>
+  )
+}
