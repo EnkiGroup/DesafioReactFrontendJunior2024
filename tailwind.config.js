d8667c9a7539633fc4b51e4379ed71eef0df5a86
Wesleyoliveira01 <Wesleyoliveira.dev@outlook.com>
2024-06-21 17:14:12 -0300

    chore(config): update tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,11 @@ module.exports = {
         default: "16px 16px 16px 60px",
       },
       boxShadow: {
-        "input-shadow-focus": " 0 -1px 2px 0 #B83F45",
+        focus: " 0 -1px 2px 0 #B83F45",
       },
-      fontFamily:{
-        'helvetica': ['Helvetica', 'sans-serif']
-      }
+      fontFamily: {
+        helvetica: ["Helvetica", "sans-serif"],
+      },
     },
   },
   plugins: [],
