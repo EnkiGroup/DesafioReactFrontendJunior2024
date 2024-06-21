3e5871f39d164aef12ecd6b6c875c3e8bd21edc2
devenzonascimento <dev.enzody@gmail.com>
2024-06-20 22:42:16 -0300

    feat: add a home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,26 @@
+import { TasksInput } from "../components/tasks-header"
+import { TasksList } from "../components/tasks-list"
+import { TasksFooter } from "../components/tasks-footer"
+
+export function Home() {
+  return (
+    <div className="h-screen bg-[#F5F5F5]">
+      <header className="flex items-center justify-center p-6">
+        <h1 className="text-7xl text-red-700">todos</h1>
+      </header>
+      <main className="h-full flex justify-center ">
+        <section className="w-[550px] h-min bg-white shadow-xl">
+          <TasksInput />
+          <TasksList />
+          <TasksFooter />
+        </section>
+      </main>
+    </div>
+  )
+}
+
+
+
+
+
+
