561593c0a06a1f397792ec83c5a7d5bb74c57104
matheusleite01 <de.leite2014@gmail.com>
2024-06-21 15:26:01 -0300

    feat: add project assets and icons folder

---
+++ src/assets/icons/CheckIcon.tsx
@@ -0,0 +1,15 @@
+const CheckIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="18"
+      viewBox="0 0 10 11"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 6.5L4 9.5L9.5 1" stroke="#69C5B5" />
+    </svg>
+  );
+};
+
+export default CheckIcon;
