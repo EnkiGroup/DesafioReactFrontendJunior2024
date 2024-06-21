2a87f1f87593c340162131f8c60064b8c422c9ad
Wesleyoliveira01 <Wesleyoliveira.dev@outlook.com>
2024-06-21 16:21:26 -0300

    feat: rendering to do list

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,40 @@
+import { ChevronDown } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import useTodoContext from "../../contexts/TodoContext";
+import { ITodo } from "../../types/todoTypes";
+import Item from "../ui/item/Item";
+
 const Main = () => {
+  const { pathname: location } = useLocation();
+  const { todos } = useTodoContext();
+  const [renderizedTodo, setRenderizedTodo] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    const filteredTodos = todos.filter((todo) => {
+      if (location === "/active") return !todo.isDone;
+      if (location === "/completed") return todo.isDone;
+      return true;
+    });
+    setRenderizedTodo(filteredTodos);
+  }, [location, todos]);
+
   return (
-    <main className="p-[16px] border border-gray-200 w-[35%]">
-      teste
+    <main className="relative border border-gray-200">
+      {renderizedTodo.length > 0 && (
+        <div className="absolute top-[-57px] left-0 w-[45px] h-[55px]">
+          <button
+            className={"w-full h-full flex justify-center items-center"}
+            name="toggle all"
+          >
+            <ChevronDown size={28} />
+          </button>
+        </div>
+      )}
+      <ul className="flex flex-col-reverse">
+        {renderizedTodo.length > 0 &&
+          renderizedTodo.map((todo) => <Item key={todo.id} todo={todo} />)}
+      </ul>
     </main>
   );
 };
