4f57e80f5d8694710fdb4fb710d02a13f8f1a34a
Wesleyoliveira01 <Wesleyoliveira.dev@outlook.com>
2024-06-21 16:36:20 -0300

    feat: implementing creation of new todo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,30 @@
+import { KeyboardEvent, useCallback, useState } from "react";
+import useTodoContext from "../../contexts/TodoContext";
 import Input from "../ui/input/Input";
 
 const Header = () => {
+  const { createTodo } = useTodoContext();
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent<HTMLInputElement>): void => {
+      if (e.key === "Enter" && newTodo.trim() !== "") {
+        createTodo(newTodo);
+        setNewTodo("");
+      }
+    },
+    [createTodo, newTodo]
+  );
+
   return (
     <header className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-title-color text-8xl">todos</h1>
       <Input
-        input_cn="focus:shadow-focus border p-default placeholder:italic placeholder:text-2xl placeholder:font-light shadow-inner"
+        input_cn="focus:shadow-focus border p-default placeholder:italic text-2xl placeholder:text-2xl placeholder:font-light shadow-inner"
         placeholder="What needs to be done?"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
     </header>
   );
