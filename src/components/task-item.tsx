a59abbb170c9020aa20a6c8ba3c86f27c5b1080a
devenzonascimento <dev.enzody@gmail.com>
2024-06-21 12:26:12 -0300

    refactor: simplify the code, and remove check button to another self component

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,11 +1,9 @@
-import { Check, X } from "lucide-react";
 import { useTasksContext } from "../contexts/tasks-context";
 
-interface Task {
-  id: string
-  title: string
-  isDone: boolean
-}
+import { CheckButton } from "./check-button";
+import { X } from "lucide-react";
+
+import { Task } from "../types";
 
 interface TaskItemProps {
   task: Task
@@ -13,51 +11,23 @@ interface TaskItemProps {
 
 export function TaskItem({ task }: TaskItemProps) {
 
-  const { toggleTaskCheck } = useTasksContext()
+  const { toggleTaskCheck, removeTask } = useTasksContext()
 
   function handleCheck() {
     toggleTaskCheck(task.id)
   }
 
+  function handleRemove() {
+    removeTask(task.id)
+  }
+
   return (
     <li className="relative flex items-center gap-4 px-2 py-4 border-t border-gray-200 peer">
-      {task.isDone
-        ?
-        (
-          <>
-            <CheckButton isDone={task.isDone} onCheck={handleCheck} />
-            <span className="text-2xl text-gray-400 line-through">
-              {task.title}
-            </span>
-            <X className="remove-icon" />
-          </>
-        )
-        :
-        (
-          <>
-            <CheckButton isDone={task.isDone} onCheck={handleCheck} />
-            <span className="text-2xl">
-              {task.title}
-            </span>
-            <X className="remove-icon" />
-          </>
-        )}
+      <CheckButton isDone={task.isDone} onCheck={handleCheck} />
+      <span className={`text-2xl ${task.isDone ? "text-gray-400 line-through" : ""}`}>
+        {task.title}
+      </span>
+      <X className="remove-icon" onClick={handleRemove} />
     </li>
   )
-}
-//TODO: A LOGICA VISUAL DO REMOVE ICON TA PRONTA, AGORA FALTA CRIAR A FUNÇÃO DE REMOVER
-export function CheckButton({ isDone, onCheck }: { isDone: boolean, onCheck: () => void }) {
-  return (
-    <label className="relative w-8 h-8 flex items-center justify-center cursor-pointer">
-      <input
-        type="checkbox"
-        checked={isDone}
-        onChange={onCheck}
-        className="sr-only peer"
-      />
-      <span className="w-8 h-8 border rounded-full flex items-center justify-center peer-checked:border-green-600 transition-all ease-in-out duration-500">
-        {isDone && <Check color="rgb(22 163 74)" />}
-      </span>
-    </label>
-  );
 }
