56e5d4d6672661bf39d6bb1b7d4af6d5db782ea5
matheusleite01 <de.leite2014@gmail.com>
2024-06-22 15:42:52 -0300

    feat: add logic to the TodoItem component

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,20 +1,54 @@
+import { useState } from "react";
 import CloseIcon from "../../assets/icons/CloseIcon";
+import useGlobalContext from "../../hooks/useGlobalContext";
 import { TaskProps } from "../../types";
 import Checkbox from "../Checkbox";
-import { TodoItemContainer, Item } from "./styles";
+import { TodoItemContainer, Item, InputEditing } from "./styles";
 
 type TodoItemProps = {
   task: TaskProps;
 };
 
 const TodoItem = ({ task }: TodoItemProps) => {
+  const { removeTask, finishTask, editingTask } = useGlobalContext();
+
+  const [isEditing, setIsEditing] = useState({
+    title: task?.title,
+    enabledEditing: false,
+  });
+
   return (
-    <TodoItemContainer>
-      <Item>
-        <Checkbox checked={task?.isDone} />
-        <span>{task?.title}</span>
+    <TodoItemContainer
+      onDoubleClick={() =>
+        setIsEditing((prev) => ({ ...prev, enabledEditing: true }))
+      }
+    >
+      <Item isEditing={task?.isDone}>
+        {!isEditing?.enabledEditing && (
+          <Checkbox
+            checked={task?.isDone}
+            handleClick={() => finishTask(task)}
+          />
+        )}
+        {isEditing?.enabledEditing ? (
+          <InputEditing
+            type="text"
+            value={isEditing?.title}
+            onChange={({ target }) =>
+              setIsEditing((prev) => ({ ...prev, title: target.value }))
+            }
+            onBlur={() => editingTask(task, isEditing?.title, setIsEditing)}
+            onKeyPress={(e) =>
+              e.key === "Enter" &&
+              editingTask(task, isEditing?.title, setIsEditing)
+            }
+            autoFocus
+          />
+        ) : (
+          <span>{task?.title}</span>
+        )}
       </Item>
-      <CloseIcon />
+      <CloseIcon handleClick={() => removeTask(task)} />
     </TodoItemContainer>
   );
 };
--- src/components/TodoItem/styles.ts
+++ src/components/TodoItem/styles.ts
@@ -1,4 +1,28 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
+
+type ItemProps = {
+  isEditing?: boolean;
+};
+
+const animaItem = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const animaSpan = keyframes`
+  from {
+    width: 0;
+  }
+  to{
+    width: 100%;
+  }
+`;
 
 export const TodoItemContainer = styled.div`
   display: flex;
@@ -9,8 +33,16 @@ export const TodoItemContainer = styled.div`
   border: 1px solid rgb(227, 227, 227);
   background: ${({ theme }) => theme.colors.white};
   cursor: pointer;
+  animation: ${animaItem} 0.2s ease-in-out;
+
+  &:hover {
+    .closeIcon {
+      display: block;
+    }
+  }
 
   .closeIcon {
+    display: none;
     color: rgba(0, 0, 0, 0.4);
     margin-right: 5px;
     transition: 0.2s;
@@ -20,13 +52,39 @@ export const TodoItemContainer = styled.div`
   }
 `;
 
-export const Item = styled.div`
+export const Item = styled.div<ItemProps>`
   display: flex;
   align-items: center;
   gap: 1.3rem;
 
   span {
+    position: relative;
     font-size: ${({ theme }) => theme.fontSize.medium};
     color: rgb(85, 85, 85);
+    white-space: nowrap;
+    ${({ isEditing }) =>
+      isEditing &&
+      css`
+        color: rgb(148, 148, 148);
+        &::after {
+          content: "";
+          display: block;
+          position: absolute;
+          top: 50%;
+          left: 0;
+          height: 2px;
+          background: rgb(148, 148, 148);
+          animation: ${animaSpan} 0.2s linear forwards;
+        }
+      `}
   }
+`;
+
+export const InputEditing = styled.input`
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  width: 100%;
+  padding: 0 3.3rem;
+  color: rgb(85, 85, 85);
+  border: none;
+  outline: none;
 `;
