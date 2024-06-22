34af5a158c226ad4eeadcba8d9228b306a518a39
devenzonascimento <dev.enzody@gmail.com>
2024-06-22 10:50:56 -0300

    refactor: move the router provider to index file, and remove home file passing it content to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet } from "react-router-dom"
 
-import { Home } from "./routes/home";
-import { AllTasks } from "./routes/all-tasks";
-import { ActiveTasks } from "./routes/active-tasks";
-import { CompletedTasks } from "./routes/completed-tasks";
+import { TasksInput } from "./components/tasks-input"
+import { TasksFooter } from "./components/tasks-footer"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    children: [
-      {
-        path: "/",
-        element: <AllTasks />
-      },
-      {
-        path: "/active",
-        element: <ActiveTasks />
-      },
-      {
-        path: "/completed",
-        element: <CompletedTasks />
-      }
-    ]
-  },
-])
-
-export function App() { 
-  return <RouterProvider router={router} />
+export function App() {
+  return (
+    <div className="h-screen flex flex-col items-center bg-[#F5F5F5]">
+      <header className="flex items-center justify-center p-6">
+        <h1 className="text-7xl text-red-700">todos</h1>
+      </header>
+      <main className="w-min flex justify-center circles-effect">
+        <section className="w-[550px] h-min bg-white custom-shadow ">
+          <TasksInput />
+          <Outlet />
+          <TasksFooter />
+        </section>
+      </main>
+      <footer>
+        <ul className="flex flex-col gap-2 mt-16 text-center text-sm text-neutral-400">
+          <li>
+            Double-click to edit a todo
+          </li>
+          <li>
+            Template by <a href="https://github.com/EnkiGroup/DesafioReactFrontendJunior2024/tree/main?tab=readme-ov-file" className="text-neutral-600 hover:underline">Best Designer</a>
+          </li>
+          <li>
+            Created by <a href="https://github.com/devenzonascimento" className="text-neutral-600 hover:underline">Enzo Nascimento</a>
+          </li>
+          <li>
+            Utilized <a href="https://react.dev/" className="text-neutral-600 hover:underline">React</a> by <a href="https://opensource.fb.com/" className="text-neutral-600 hover:underline">Meta</a>
+          </li>
+          <li>
+            Part of <a href="https://todomvc.com/" className="text-neutral-600 hover:underline">TodoMVC</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,45 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { TasksProvider } from './contexts/tasks-context.tsx'
 
 import { App } from './App.tsx'
+import { AllTasks } from "./routes/all-tasks";
+import { ActiveTasks } from "./routes/active-tasks";
+import { CompletedTasks } from "./routes/completed-tasks";
 
 import "./index.css"
 
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <AllTasks />
+      },
+      {
+        path: "/active",
+        element: <ActiveTasks />
+      },
+      {
+        path: "/completed",
+        element: <CompletedTasks />
+      }
+    ]
+  },
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <TasksProvider>
-        <App />
+        <RouterProvider router={router} />
       </TasksProvider>
     </QueryClientProvider>
   </StrictMode>,
