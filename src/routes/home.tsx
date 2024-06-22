59833273069f1acece9e27b00cbfbe78609a61f1
devenzonascimento <dev.enzody@gmail.com>
2024-06-22 01:17:06 -0300

    feat: add links on footer

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,34 +4,35 @@ import { TasksInput } from "../components/tasks-input"
 import { TasksFooter } from "../components/tasks-footer"
 
 export function Home() {
+
   return (
-    <div className="h-screen flex flex-col gap-8 bg-[#F5F5F5]">
-      <header className="flex items-center justify-center pt-6">
+    <div className="h-screen flex flex-col items-center bg-[#F5F5F5]">
+      <header className="flex items-center justify-center p-6">
         <h1 className="text-7xl text-red-700">todos</h1>
       </header>
-      <main className="flex justify-center ">
-        <section className="w-[550px] h-min bg-white custom-shadow">
+      <main className="w-min flex justify-center circles-effect">
+        <section className="w-[550px] h-min bg-white custom-shadow ">
           <TasksInput />
           <Outlet />
           <TasksFooter />
         </section>
       </main>
       <footer>
-        <ul className="flex flex-col gap-2 text-center text-sm text-neutral-400">
+        <ul className="flex flex-col gap-2 mt-16 text-center text-sm text-neutral-400">
           <li>
             Double-click to edit a todo
           </li>
           <li>
-            Template by <strong className="text-neutral-600 hover:underline">Sindre Sorhus</strong>
+            Template by <a href="https://github.com/EnkiGroup/DesafioReactFrontendJunior2024/tree/main?tab=readme-ov-file" className="text-neutral-600 hover:underline">Best Designer</a>
           </li>
           <li>
-            Created by <strong className="text-neutral-600 hover:underline">Chau Tran</strong>
+            Created by <a href="https://github.com/devenzonascimento" className="text-neutral-600 hover:underline">Enzo Nascimento</a>
           </li>
           <li>
-            Utilized <strong className="text-neutral-600 hover:underline">XState</strong> by <strong className="text-neutral-600 hover:underline">David K</strong>
+            Utilized <a href="https://react.dev/" className="text-neutral-600 hover:underline">React</a> by <a href="https://opensource.fb.com/" className="text-neutral-600 hover:underline">Meta</a>
           </li>
           <li>
-            Part of <strong className="text-neutral-600 hover:underline">TodoMVC</strong>
+            Part of <a href="https://todomvc.com/" className="text-neutral-600 hover:underline">TodoMVC</a>
           </li>
         </ul>
       </footer>
@@ -39,8 +40,5 @@ export function Home() {
   )
 }
 
-
-
-
-
-
+//TODO: CRIAR OS CIRCULAS ATRÁS DA SECTION
+//TODO: FAZER ANIMAÇÃO COM O SHEETS EFFECT
