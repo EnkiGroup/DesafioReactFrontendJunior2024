39a1d344b5a63f81ed5eb6b2e3cdb74a4f091ae5
Henrique Santiago <henrique11santiagopires@hotmail.com>
2024-06-22 15:49:05 -0300

    Separating api fetch in a new folder

--- src/hooks/useFetchTodos.ts
+++ src/hooks/useFetchTodos.ts
@@ -1,18 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
+import { fetchTodos } from "../utils/api";
 
 export default function useFetchTodos(){
 
-    const TODO_URL = "https://my-json-server.typicode.com/EnkiGroup/DesafioReactFrontendJunior2024/todos"
-
     const { data: todoFetch, error, isLoading} = useQuery({
         queryKey: ['todos'],
-        queryFn: async () => {
-            const response = await fetch(TODO_URL);
-            if (!response.ok) {
-                throw new Error('Erro ao buscar os todos');
-            }
-            return response.json();
-        },
+        queryFn: fetchTodos,
         refetchOnWindowFocus: false,
         refetchOnMount: false,
         retry: 3
