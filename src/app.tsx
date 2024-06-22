e595cfe4a8fd89f5c70906c44c240d1614f9f6d8
CarolGonzaga <dev.carolgonzaga@gmail.com>
2024-06-22 17:39:04 -0300

    Tarefa 1 - Recuperando as tarefas via GET

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
+interface Todo {
+  id: string;
+  title: string;
+  completed: boolean;
+}
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://my-json-server.typicode.com/EnkiGroup/DesafioReactFrontendJunior2024/todos');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setTodos(data);
+      } catch (err) {
+        console.error('Failed to fetch todos:', err);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
   return (
     <section>
       <h1>Todos</h1>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.title} - {todo.completed ? 'Completed' : 'Not Completed'}
+          </li>
+        ))}
+      </ul>
     </section>
   );
-}
+};
+
+export default App;
