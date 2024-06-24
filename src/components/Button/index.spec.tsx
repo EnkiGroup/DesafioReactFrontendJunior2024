7744a7144ab9ce44bbfd92c719fb019123d5bb12
matheusleite01 <de.leite2014@gmail.com>
2024-06-24 15:12:07 -0300

    feat: add test for button component

---
+++ src/components/Button/index.spec.tsx
@@ -0,0 +1,45 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "../../utils/renderWithTheme";
+import { BrowserRouter } from "react-router-dom";
+import Button from ".";
+
+describe("Button component", () => {
+  it("shold render component", () => {
+    renderWithTheme(
+      <BrowserRouter>
+        <Button to={"about"}>ativar</Button>
+      </BrowserRouter>,
+    );
+
+    const buttonLink = screen.getByRole("link", { name: /ativar/i });
+
+    expect(buttonLink).toBeInTheDocument();
+  });
+
+  it("Shold have a link /about", () => {
+    renderWithTheme(
+      <BrowserRouter>
+        <Button to={"about"}>ativar</Button>
+      </BrowserRouter>,
+    );
+
+    const buttonLink = screen.getByRole("link", { name: /ativar/i });
+
+    expect(buttonLink).toBeInTheDocument();
+    expect(buttonLink).toHaveAttribute("href", "/about");
+  });
+
+  it("Shold have border color when active is passed", () => {
+    renderWithTheme(
+      <BrowserRouter>
+        <Button className={"active"} to={"about"}>
+          ativar
+        </Button>
+      </BrowserRouter>,
+    );
+
+    const buttonLink = screen.getByRole("link", { name: /ativar/i });
+
+    expect(buttonLink).toHaveStyle({ border: `1px solid #b83f45` });
+  });
+});
