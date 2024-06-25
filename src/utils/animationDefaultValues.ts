9c99fbdd7a1484e04e8cd5f7e07051a7480b58ab
matheusleite01 <de.leite2014@gmail.com>
2024-06-25 17:51:24 -0300

    feat: add classname option to animationDefaultValues

--- src/utils/animationDefaultValues.ts
+++ src/utils/animationDefaultValues.ts
@@ -1,10 +1,11 @@
-export const animationDefaultValues = (animation: object) => {
+export const animationDefaultValues = (animation: object, className?: string) => {
   return {
     loop: false,
     autoplay: true,
     animationData: animation,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
+      className: className,
     },
   };
 };
