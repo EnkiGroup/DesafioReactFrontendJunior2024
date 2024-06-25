b51fb66e9d1f4f8662d1da0961cba40106472f4f
matheusleite01 <de.leite2014@gmail.com>
2024-06-25 14:11:19 -0300

    feat: add toast icon treatment

--- src/components/Toast/styled.ts
+++ src/components/Toast/styled.ts
@@ -2,21 +2,38 @@ import styled from "styled-components";
 
 export const ToastContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: end;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const MessageContainer = styled(ToastContainer)`
   gap: 10px;
 `;
 
 export const ButtonContainer = styled(ToastContainer)`
-  flex-direction: row;
+  flex-direction: column;
+  align-items: end;
   gap: 5px;
 
   button {
-    color: #111827;
-    background: #f3f4f6;
-    padding: 4px 8px;
+    color: ${({ theme }) => theme.colors.black};
+    background: ${({ theme }) => theme.colors.gray_4};
+    padding: 2px 8px;
     border: 1px solid transparent;
     border-radius: 5px;
     cursor: pointer;
+    transform: translateY(0);
+    transition: all.2s;
+
+    &:first-child {
+      background: ${({ theme }) => theme.colors.blueIcon};
+      font-weight: 600;
+      color: ${({ theme }) => theme.colors.white};
+    }
+
+    &:hover {
+      transform: translateY(4px);
+    }
   }
 `;
