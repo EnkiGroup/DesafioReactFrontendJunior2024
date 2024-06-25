8d1ec01c632663238720b02e95dac89a7ce4a0ab
matheusleite01 <de.leite2014@gmail.com>
2024-06-25 18:12:38 -0300

    feat: added description to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,14 +3,23 @@ import FooterConteiner from "./styled";
 const Footer = () => {
   return (
     <FooterConteiner>
-      <p>Double-click para editar a tarefa</p>
-      <p>Desenvolvido por</p>
+      <p>Double-click to edit a todo</p>
+      <p>
+        Template by{" "}
+        <a
+          href="https://github.com/EnkiGroup/DesafioReactFrontendJunior2024"
+          target="_blank"
+        >
+          EnkiGroup
+        </a>
+      </p>
+      <p>Created by</p>
       <a
         href="https://github.com/matheusleite01"
         target="_blank"
         rel="noreferrer"
       >
-        @Matheus Leite 🖤
+        @Matheus Leite ❤️
       </a>
     </FooterConteiner>
   );
