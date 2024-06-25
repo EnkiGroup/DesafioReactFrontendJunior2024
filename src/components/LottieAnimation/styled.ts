6d2d5d4b7c002383419106ef64e3908ce93c62db
matheusleite01 <de.leite2014@gmail.com>
2024-06-25 17:54:47 -0300

    feat: adjust reponsive for lottieAnimation

--- src/components/LottieAnimation/styled.ts
+++ src/components/LottieAnimation/styled.ts
@@ -4,6 +4,16 @@ const AnimaContainer = styled.div`
   position: relative;
   top: 200px;
   pointer-events: none;
+
+  @media screen and (max-width: 450px){
+    top: 250px;
+    right: -35px;
+    .lottie-svg-class-bg{
+      width: 400px !important;
+      height: 400px !important;
+    }
+  }
+
 `;
 
 export default AnimaContainer;
