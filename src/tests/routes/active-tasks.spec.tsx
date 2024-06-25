a40a31080ad3e7de22a577c356659905ab72d510
devenzonascimento <dev.enzody@gmail.com>
2024-06-24 23:36:31 -0300

    refactor: organize code

--- src/tests/routes/active-tasks.spec.tsx
+++ src/tests/routes/active-tasks.spec.tsx
@@ -9,35 +9,29 @@ jest.mock('../../contexts/tasks-context', () => ({
   useTasksContext: jest.fn(),
 }));
 
-const tasks = [
-  {
-    "id": "flrGI",
-    "title": "Lavar os pratos",
-    "isDone": false
-  },
-  {
-    "id": "Tw-I9",
-    "title": "Cortar a grama",
-    "isDone": true
-  },
-  {
-    "id": "7f2sf",
-    "title": "Comprar pão",
-    "isDone": false
-  }
-]
-const mockContextValue = {
-  tasks: tasks
-};
-
-(useTasksContext as jest.Mock).mockReturnValue(mockContextValue);
-
 describe("ActiveTasks", () => {
+  const tasks = [
+    {
+      "id": "flrGI",
+      "title": "Lavar os pratos",
+      "isDone": false
+    },
+    {
+      "id": "Tw-I9",
+      "title": "Cortar a grama",
+      "isDone": true
+    }
+  ]
+  const mockContextValue = {
+    tasks: tasks
+  };
+  
+  (useTasksContext as jest.Mock).mockReturnValue(mockContextValue);
+
   it("should render only active tasks", () => {
     render(<ActiveTasks />)
 
     expect(screen.queryByText(tasks[0].title)).toBeInTheDocument()
     expect(screen.queryByText(tasks[1].title)).not.toBeInTheDocument()
-    expect(screen.queryByText(tasks[2].title)).toBeInTheDocument()
   })
 })
--- src/tests/routes/all-tasks.spec.tsx
+++ src/tests/routes/all-tasks.spec.tsx
@@ -9,35 +9,30 @@ jest.mock('../../contexts/tasks-context', () => ({
   useTasksContext: jest.fn(),
 }));
 
-const tasks = [
-  {
-    "id": "flrGI",
-    "title": "Lavar os pratos",
-    "isDone": false
-  },
-  {
-    "id": "Tw-I9",
-    "title": "Cortar a grama",
-    "isDone": true
-  },
-  {
-    "id": "7f2sf",
-    "title": "Comprar pão",
-    "isDone": false
-  }
-]
-const mockContextValue = {
-  tasks: tasks
-};
-
-(useTasksContext as jest.Mock).mockReturnValue(mockContextValue);
 
 describe("AllTasks", () => {
+  const tasks = [
+    {
+      "id": "flrGI",
+      "title": "Lavar os pratos",
+      "isDone": false
+    },
+    {
+      "id": "Tw-I9",
+      "title": "Cortar a grama",
+      "isDone": true
+    }
+  ]
+  const mockContextValue = {
+    tasks: tasks
+  };
+  
+  (useTasksContext as jest.Mock).mockReturnValue(mockContextValue);
+  
   it("should render all tasks correctly", async () => {
     render(<AllTasks />)
 
     expect(screen.queryByText(tasks[0].title)).toBeInTheDocument()
     expect(screen.queryByText(tasks[1].title)).toBeInTheDocument()
-    expect(screen.queryByText(tasks[2].title)).toBeInTheDocument()
   })
 })
