bab32e19c6a09924cf11f452c17a496296128118
matheusleite01 <de.leite2014@gmail.com>
2024-06-25 18:14:17 -0300

    feat: added style to link

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -5,15 +5,24 @@ const FooterConteiner = styled.footer`
   flex-direction: column;
   align-items: center;
   text-align: center;
-  gap: 5px;
-  margin-top: 2.5rem;
-  font-size: 13px;
-  color: ${({ theme }) => theme.colors.gray_3};
+  gap: 10px;
+  margin-top: 3.5rem;
+  font-size: 14px;
+  color: #a3a3a3;
 
   a {
     font-size: 1rem;
     color: ${({ theme }) => theme.colors.red};
     margin-top: 5px;
+
+    &:first-child {
+      font-size: 13px;
+      color: ${({ theme }) => theme.colors.black};
+    }
+
+    &:hover{
+      text-decoration: underline;
+    }
   }
 `;
 
