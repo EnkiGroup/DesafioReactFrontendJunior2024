3bf3cc35971adce085d4901dacc1696f79f1d265
Alisson <binho_alisson@hotmail.com>
2024-06-25 17:31:00 -0300

    feat: updating react version,  adding react redux and redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
-import reportWebVitals from "./reportWebVitals";
+import App from './app';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root container not found');
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
